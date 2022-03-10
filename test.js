04f506735f6d2e58892a0f0d650cfc0583443264
reza rezaee <r871017h800501@gmail.com>
2022-03-11 02:11:36 +0330

    navbar practice ,list ,conditions

--- test.js
+++ test.js
@@ -1,42 +1,10 @@
 'use strict';
+var color_list=[
+    'red',
+    'tomato',
+    'green',
+    'black',
+]
 
-
-let obj = {
-    name:'reza',
-    func1(){
-        console.log('***func 1');
-    },
-    func2: function(){
-        this.func1()
-        // console.log(this)
-        // console.log('***func 2');
-    },
-    arrofunc:()=>{
-        console.log(this)
-        // console.log('***arrow func 3');
-    },
-
-    arrofunc2: () => console.log(this.i, this),
-
-    manualfunc4:function manualfunc4(){
-        this.func2()
-        // this.arrofunc()
-        // console.log(this)
-        // console.log('***manual func 4');
-    },
-
-    mainfunc(){
-        // this.func1()
-        // this.func2()
-        // this.arrofunc()
-        this.manualfunc4()
-        // console.log(this)
-    }
-}
-
-obj.func1()
-obj.manualfunc4()
- 
-
-
+console.log(color_list[1])
 
