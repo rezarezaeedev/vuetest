226ead3a048d731f8f7012182961c2b88e6baea7
reza rezaee <r871017h800501@gmail.com>
2022-04-10 06:49:13 +0330

    improved paginator to better, now show three page less and more than current page

--- vue-blog/src/components/paginator-component.vue
+++ vue-blog/src/components/paginator-component.vue
@@ -31,6 +31,12 @@ export default {
            }
            return res
        },
+
+       getRangePage(){
+           let min = Math.max(1, this.currentPageNumber-3)
+           let max = Math.min(this.length, this.currentPageNumber+3)
+           return Array.range(min, max, 1)
+       }
    }
 }
 </script>
@@ -41,12 +47,7 @@ export default {
         <ul class="pagination justify-content-center my-4" v-if="length">
             <li class="page-item mx-4"><a class="page-link"  :href="getPageUrl(1)"  @click.prevent='updateCurrentPage(1)'>First</a></li>
             <li class="page-item"><a class="page-link"  :href="getPageUrl(prepageNumber())"  @click.prevent='updateCurrentPage(prepageNumber())'>Previous</a></li>
-
-            <!-- <li class="page-item" v-for="(page, index) in length" :class="{'active':page==currentPageNumber}"  :key="index" ><a class="page-link" :href="getPageUrl(page)" v-text="page" @click.prevent='updateCurrentPage(page)'></a></li> -->
-            <li class="page-item"><a class="page-link"  :href="getPageUrl(prepageNumber())" v-if="currentPageNumber-1 > 0"  @click.prevent='updateCurrentPage(currentPageNumber-1)'>{{currentPageNumber-1}}</a></li>
-            <li class="page-item active" ><a class="page-link"  v-text="currentPageNumber" @click.prevent='updateCurrentPage(page)'></a></li>
-            <li class="page-item"><a class="page-link"  :href="getPageUrl(prepageNumber())" v-if="currentPageNumber+1<length"  @click.prevent='updateCurrentPage(currentPageNumber+1)'>{{currentPageNumber+1}}</a></li>
-
+            <li class="page-item" v-for="(page, index) in getRangePage()" :class="{'active':page==currentPageNumber}"  :key="index" ><a class="page-link" :href="getPageUrl(page)" v-text="page" @click.prevent='updateCurrentPage(page)'></a></li>
             <li class="page-item"><a class="page-link"   tabindex="-1" aria-disabled="true"  :href="getPageUrl(nextpageNumber())" @click.prevent='updateCurrentPage(nextpageNumber())'>Next</a></li>
             <li class="page-item mx-4"><a class="page-link"   tabindex="-1" aria-disabled="true"  :href="getPageUrl(length)" @click.prevent='updateCurrentPage(length)'>Last</a></li>
         </ul>
---
+++ vue-blog/test.js
@@ -0,0 +1,9 @@
+function log(...rest){
+    console.log(...rest)
+}
+
+let obj = {name:'reza', age:22}
+obj.n=12
+obj['55'] = 'ss'
+log(obj)
+
